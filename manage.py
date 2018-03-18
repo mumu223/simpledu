8307ffda95c2eae5b54229c237c60d4745ce8fec
mumu223 <sszhao223@163.c0m>
2018-03-18 10:56:02 +0800

    manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,12 @@
 from simpledu.app import create_app
-app=create_app('development')
-if __name__== '__main__':
-    app.run()
+
+app = create_app('development')
+
+
+if __name__ == '__main__':
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
+
 
