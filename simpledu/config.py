b372c61efd3a6e1d3dce7685f929a554528f8ce1
mumu223 <sszhao223@163.c0m>
2018-03-18 11:04:43 +0800

    base

--- simpledu/config.py
+++ simpledu/config.py
@@ -1,14 +1,23 @@
 class BaseConfig(object):
-    """peizhijilei"""
-    SECRET_KEY='makesure to set a very secret key'
+    SECRET_KEY = 'very secret key'
+    INDEX_PER_PAGE = 9
+    ADMIN_PER_PAGE = 15
+
 class DevelopmentConfig(BaseConfig):
-     """kaifahuanjngpeizhi"""
-    DEBUG=True
-    SQLALCHEMY_DATABASE_URI='mysql+mysqldb://root@localhost:3306/simpledu?charset=utf8'
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/simpledu?charset=utf8'
+
+
 class ProductionConfig(BaseConfig):
-    """shengchanhuanjingpeizhi"""
     pass
-configs={
-    'development':DevelopmentConfig,
-    'production':ProductionConfig,
-    'testing':TestingConfig}
+
+
+class TestingConfig(BaseConfig):
+    pass
+
+
+configs = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
+}
